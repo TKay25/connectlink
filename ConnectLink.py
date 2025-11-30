21468705bce94f9bad0726359808dac44ff592c2
TEEKAYZ <99164597+TEEKAYZ@users.noreply.github.com>
2025-11-30 14:14:42 +0200

    ss

--- ConnectLink.py
+++ ConnectLink.py
@@ -72,11 +72,11 @@ def initialize_database_tables():
                 );
             """)
             current_date = datetime.now().strftime('%Y-%m-%d')
-            cursor.execute("""
+            '''cursor.execute("""
                 INSERT INTO connectlinkusers (datecreated, name, password, email)
                 VALUES (%s, %s, %s, %s);
             """, (current_date, "ConnectLinkAdmin01", "ConnectLinkAdmin01", "connectlink@gmail.com"))
-
+            '''
 
             # Create connectlinkadmin table
             cursor.execute("""
