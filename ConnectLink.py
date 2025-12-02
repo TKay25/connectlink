ee0ba9a89516d697b64e84147b00e0f6f0d8e03f
TKay25 <echelonequipmentimports@gmail.com>
2025-12-02 22:14:36 +0200

    Update ConnectLink.py

--- ConnectLink.py
+++ ConnectLink.py
@@ -411,7 +411,7 @@ def download_contract(project_id):
                 'installment6amount': row[41] if len(row) > 41 else None,
                 'installment6duedate': row[42].strftime("%-d %B %Y") if len(row) > 42 else None,
                 'installment6date': row[43].strftime("%-d %B %Y") if len(row) > 43 else None,
-                "latepaymentinterest": row[44] if len(row) > 43 else None,
+                "latepaymentinterest": row[45] if len(row) > 43 else None,
                 'companyname': companyname,
                 'companyaddress': address,
                 'companycontact1': contact1,
