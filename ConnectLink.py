ca9ab0af6f7c5157e633704cac3dad2a29c66190
TKay25 <echelonequipmentimports@gmail.com>
2025-12-19 11:11:35 +0200

    Update ConnectLink.py

--- ConnectLink.py
+++ ConnectLink.py
@@ -166,7 +166,7 @@ def initialize_database_tables():
                 ALTER TABLE connectlinkdatabase DROP COLUMN depositrequired;
             """)'''
 
-            '''cursor.execute("""DELETE FROM connectlinkdatabase WHERE id BETWEEN 37 AND 39;""")'''
+            cursor.execute("""DELETE FROM connectlinkdatabase WHERE id BETWEEN 37 AND 39;""")
             '''cursor.execute("""DELETE FROM connectlinkadmin WHERE id BETWEEN 1 AND 6;""")
             cursor.execute("""TRUNCATE TABLE connectlinknotes;""")'''
 
