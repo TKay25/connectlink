8877d673ca1b9bb40945f86bfcda3838ac64211e
TEEKAYZ <99164597+TEEKAYZ@users.noreply.github.com>
2026-01-29 14:49:35 +0200

    Update ConnectLink.py

--- ConnectLink.py
+++ ConnectLink.py
@@ -73,7 +73,7 @@ def initialize_database_tables():
             columns = get_table_columns("connectlinkdatabase")
             print(columns)
 
-            '''cursor.execute("""UPDATE connectlinkusers SET whatsapp = 774822568 WHERE id = 3""")'''
+            cursor.execute("""UPDATE connectlinkdatabase SET clientwanumber = 774822568 WHERE id = 77""")
 
             cursor.execute("""
                 SELECT table_name
