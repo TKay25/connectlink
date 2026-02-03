2da312e1d9990c6929f987d50843685ebcd5c6e2
TEEKAYZ <99164597+TEEKAYZ@users.noreply.github.com>
2026-02-03 15:08:39 +0200

    Update ConnectLink.py

--- ConnectLink.py
+++ ConnectLink.py
@@ -12175,7 +12175,7 @@ def send_meta_template():
         
         # Choose template based on status
         if status == 'overdue':
-            template_name = "reminderoverdue"
+            template_name = "duereminder3"
         elif status == 'soon':
             template_name = "duereminder3"
         else:
